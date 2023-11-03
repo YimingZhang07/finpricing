9dc324fce3026494db828807d4005d25ffdb1b40
YimingZhang07 <46825198+YimingZhang07@users.noreply.github.com>
2023-11-03 22:23:56 +0000

    fix discount curve

--- src/finpricing/market/discount_curve_zero.py
+++ src/finpricing/market/discount_curve_zero.py
@@ -1,61 +1,165 @@
-import numpy as np
 from typing import Union
 import datetime
-from ..utils.interpolator import InterpTypes, Interpolator
+import math
+from bisect import bisect_left
+from ..utils.interpolator import InterpTypes
 from ..utils.day_count import DayCountTypes, DayCount
 from ..utils.date import Date
 
+
 class DiscountCurve:
     def __init__(self) -> None:
-        pass  
-    
-    def discount(date: Date):
+        pass
+
+    def discount(self, date: Union[Date, datetime.date]):
+        """discount factor for date"""
         return NotImplementedError("Please implement factor() method in derived class")
-    
-    
-class DiscountCurveZeroRates(DiscountCurve):
-    
+
+
+class FlatForwardInterpolator:
     def __init__(self,
-                 anchor_date = None,
+                 anchor_date: Union[Date, datetime.date],
+                 spot_date,
+                 dates,
+                 rates,
+                 day_counter,
+                 continuous_compounding):
+        self.anchor_date = anchor_date
+        self.spot_date = spot_date
+        self.day_counter = day_counter
+        self.dates = dates
+        self.rates = rates
+        self.times = self.day_counter.convert_dates_to_times(self.anchor_date,
+                                                             [self.anchor_date] + self.dates)
+        self.times_relateive_to_spot = [t - self.day_counter.year_fraction(self.anchor_date, self.spot_date) for t in self.times]
+        self.forwards = []
+        self.cum_factors = []
+        if continuous_compounding:
+            self.run_continuous_flat_forward_rates()
+            self.dicount_handle = self.continuous_discount
+        else:
+            self.run_annual_compounding()
+            self.dicount_handle = self.annual_discount
+
+    def run_continuous_flat_forward_rates(self):
+        """"derive forward rates using ontinuous compounding"""
+        for i, _ in enumerate(self.times_relateive_to_spot):
+            if i == 0:
+                self.forwards.append(self.rates[0])
+                self.cum_factors.append(1.)
+            elif i == len(self.times_relateive_to_spot) - 1:
+                self.cum_factors.append(
+                    self.cum_factors[-1] * math.exp(-1 * self.forwards[-1] * (self.times[i] - self.times[i-1])))
+                self.forwards.append(self.forwards[-1])
+            else:
+                r1 = self.rates[i - 1]
+                r2 = self.rates[i]
+                t1 = self.times_relateive_to_spot[i]
+                t2 = self.times_relateive_to_spot[i + 1]
+                f = -math.log(math.exp(-t2 * r2) /
+                              math.exp(-t1 * r1)) / (t2 - t1)
+                self.cum_factors.append(
+                    self.cum_factors[-1] * math.exp(-1 * self.forwards[-1] * (self.times[i] - self.times[i-1])))
+                self.forwards.append(f)
+
+    def continuous_discount(self, dt):
+        """"discount using continuous compounding"""
+        idx = max(min(bisect_left(self.times, dt) - 1, len(self.times) - 1), 0)
+        return self.cum_factors[idx] * math.exp(-self.forwards[idx] * (dt - self.times[idx]))
+
+    def _annual_discount(self, rate, time):
+        return 1 / (1 + rate) ** time
+
+    def run_annual_compounding(self):
+        """derive forward rates using annual compounding"""
+        for i, _ in enumerate(self.times_relateive_to_spot):
+            if i == 0:
+                self.forwards.append(self.rates[0])
+                self.cum_factors.append(1.)
+            elif i == len(self.times_relateive_to_spot) - 1:
+                self.cum_factors.append(self.cum_factors[-1] * self._annual_discount(
+                    self.forwards[-1], self.times[i] - self.times[i-1]))
+                self.forwards.append(self.forwards[-1])
+            else:
+                r1 = self.rates[i - 1]
+                r2 = self.rates[i]
+                t1 = self.times_relateive_to_spot[i]
+                t2 = self.times_relateive_to_spot[i + 1]
+                f = (self._annual_discount(r1, t1) /
+                     self._annual_discount(r2, t2)) ** (1/(t2 - t1)) - 1
+                self.cum_factors.append(self.cum_factors[-1] * self._annual_discount(
+                    self.forwards[-1], self.times[i] - self.times[i-1]))
+                self.forwards.append(f)
+
+    def annual_discount(self, dt):
+        """discount using annual compounding"""
+        idx = max(min(bisect_left(self.times, dt) - 1, len(self.times) - 1), 0)
+        return self.cum_factors[idx] * self._annual_discount(self.forwards[idx], dt - self.times[idx])
+
+    def eval(self, dt):
+        """"evaluate discount factor for dt using the correct discounting method"""
+        return self.dicount_handle(dt)
+
+
+class DiscountCurveZeroRates(DiscountCurve):
+
+    def __init__(self,
+                 anchor_date=None,
                  dates: list[float] = None,
                  rates: list[float] = None,
-                 interp_type = InterpTypes.FLAT_FWD_RATES,
+                 spot_date=None,
+                 continuous_compounding: bool = True,
+                 interp_type=InterpTypes.FLAT_FWD_RATES,
                  day_count_type: DayCountTypes = DayCountTypes.ACT_365) -> None:
         self.anchor_date = anchor_date
         self.dates = dates
         self.rates = rates
         self.interp_type = interp_type
         self.day_count_type = day_count_type
-        
+        if spot_date is None:
+            self.spot_date = self.anchor_date
+        else:
+            self.spot_date = spot_date
         # derived
-        self.day_counter    = DayCount(self.day_count_type)
-        self.times          = self.day_counter.convert_dates_to_times(self.anchor_date, self.dates)
-        self.factors        = np.exp( -np.multiply(self.times, self.rates) ).tolist()
-        self.interpolator   = Interpolator([0.] + self.times, [1.] + self.factors, self.interp_type)
-        
-        
+        self.day_counter = DayCount(self.day_count_type)
+        if interp_type == InterpTypes.FLAT_FWD_RATES:
+            self.interpolator = FlatForwardInterpolator(
+                self.anchor_date,
+                self.spot_date,
+                self.dates,
+                self.rates,
+                self.day_counter,
+                continuous_compounding=continuous_compounding
+            )
+        else:
+            raise NotImplementedError(
+                "This interp_type has not been supported.")
+
     def discount(self, date: Date):
-        dt                  = self.day_counter.year_frac(self.anchor_date, date)
+        dt = self.day_counter.year_fraction(self.anchor_date, date)
         return self.interpolator.eval(dt)
-    
+
 
 class DiscountCurveZeroShifted:
     def __init__(self,
                  underlying_curve: DiscountCurveZeroRates,
                  alpha: float,
-                 beta: float=1.0,
-                 day_count_type: DayCountTypes=DayCountTypes.ACT_365) -> None:
+                 beta: float = 1.0,
+                 day_count_type: DayCountTypes = DayCountTypes.ACT_365) -> None:
         self.underlying_curve = underlying_curve
         self.alpha = alpha
         self.beta = beta
         self.day_count_type = day_count_type
         # derived
-        self.day_counter    = DayCount(self.day_count_type)
-        assert self.day_count_type == self.underlying_curve.day_count_type, "DiscountCurveZeroShifted: day_count_type must be the same as underlying_curve.day_count_type"
-        
+        self.day_counter = DayCount(self.day_count_type)
+        assert self.day_count_type == self.underlying_curve.day_count_type, \
+            "Day_count_type must be the same as underlying_curve.day_count_type"
+
     def discount(self, date: Union[Date, datetime.date]):
+        """discount factor for date"""
         factor = self.underlying_curve.discount(date)
-        time   = self.day_counter.year_fraction(self.underlying_curve.anchor_date, date)
-        factor_adjusted = np.power(factor, self.beta) * np.exp(-self.alpha * time)
+        time = self.day_counter.year_fraction(
+            self.underlying_curve.anchor_date, date)
+        factor_adjusted = math.pow(
+            factor, self.beta) * math.exp(-self.alpha * time)
         return factor_adjusted
-        
--- tests/test_discount_curve_zero.py
+++ tests/test_discount_curve_zero.py
@@ -5,55 +5,113 @@ import os
 import pickle
 
 def test_discount():
-    
     anchor_date = datetime.date(2023, 10, 9)
-    
-    dates       = [
-        datetime.date(2023, 12, 31),
-        datetime.date(2024, 12, 31),
-    ]
-    
-    rates      = [
-        .05,
-        .06
-    ]
-    
+    dates       = [datetime.date(2023, 12, 31), datetime.date(2024, 12, 31)] 
+    rates       = [.05, .06]
     discountCurve = DiscountCurveZeroRates(anchor_date, dates, rates)
+    assert discountCurve.discount(datetime.date(2023, 10, 9))   == pytest.approx(1.)
+    assert discountCurve.discount(datetime.date(2023, 10, 10))  == pytest.approx(.9998630230808251)
+    assert discountCurve.discount(datetime.date(2023, 10, 31))  == pytest.approx(.9969908380010889)
+    assert discountCurve.discount(datetime.date(2025, 10, 31))  == pytest.approx(.8819061927598262)
+
+def test_forwards():
+    anchor_date = datetime.date(2023, 10, 9)
+    dates = [datetime.date(2023, 10, 11), datetime.date(2023, 10, 31), datetime.date(2023, 12, 31)]
+    rates = [0.05, 0.07, 0.09]
+    discountCurve = DiscountCurveZeroRates(anchor_date, dates, rates, spot_date=datetime.date(2023, 12, 15))
+    assert discountCurve.interpolator.forwards[0]   == 0.05
+    assert discountCurve.interpolator.forwards[1]   == pytest.approx(0.005)
+    assert discountCurve.interpolator.forwards[2]   == pytest.approx(0.07524590163934353)
+    assert discountCurve.interpolator.forwards[3]   == pytest.approx(0.07524590163934353)
+    assert len(discountCurve.interpolator.forwards) == 4
+
+    assert discountCurve.interpolator.cum_factors[0] == pytest.approx(1.)
+    assert discountCurve.interpolator.cum_factors[1] == pytest.approx(0.9997260649243265)
+    assert discountCurve.interpolator.cum_factors[2] == pytest.approx(0.9994522048890787)
+    assert discountCurve.interpolator.cum_factors[3] == pytest.approx(0.986962447217427)
+    assert len(discountCurve.interpolator.cum_factors) == 4
+
+    assert discountCurve.discount(datetime.date(2023, 10, 9))   == pytest.approx(1.)
+    assert discountCurve.discount(datetime.date(2023, 11, 12))  == pytest.approx(0.9969827779235936)
+    assert discountCurve.discount(datetime.date(2023, 12, 23))  == pytest.approx(0.9885915135771349)
+    assert discountCurve.discount(datetime.date(2024, 1, 2))    == pytest.approx(0.9865556002509076)
+
+def test_forwards_annual_compounding():
+    anchor_date = datetime.date(2023, 10, 9)
+    dates = [datetime.date(2023, 10, 11), datetime.date(2023, 10, 31), datetime.date(2023, 12, 31)]
+    rates = [0.05, 0.07, 0.09]
+    discountCurve = DiscountCurveZeroRates(anchor_date, dates, rates, spot_date=datetime.date(2023, 12, 15), continuous_compounding=False)
+    assert discountCurve.interpolator.forwards[0]   == 0.05
+    assert discountCurve.interpolator.forwards[1]   == pytest.approx(0.00635618986625186)
+    assert discountCurve.interpolator.forwards[2]   == pytest.approx(0.07521012073226463)
+    assert discountCurve.interpolator.forwards[3]   == pytest.approx(0.07521012073226463)
+    assert len(discountCurve.interpolator.forwards) == 4
+
+    assert discountCurve.interpolator.cum_factors[0] == pytest.approx(1.)
+    assert discountCurve.interpolator.cum_factors[1] == pytest.approx(0.9997326923677412)
+    assert discountCurve.interpolator.cum_factors[2] == pytest.approx(0.9993856632532903)
+    assert discountCurve.interpolator.cum_factors[3] == pytest.approx(0.9873470747853555)
+    assert len(discountCurve.interpolator.cum_factors) == 4
+
+    assert discountCurve.discount(datetime.date(2024, 1, 2))    == pytest.approx(0.9869548318270954)
     
-    assert discountCurve.discount(datetime.date(2023, 10, 9)) == pytest.approx(1.)
-    assert discountCurve.discount(datetime.date(2023, 10, 10)) == pytest.approx(.9998630230808251)
-    assert discountCurve.discount(datetime.date(2023, 10, 31)) == pytest.approx(.9969908380010889)
-    assert discountCurve.discount(datetime.date(2025, 10, 31)) == pytest.approx(.8819061927598262)
-    
-def test_discount_2():
+def test_real_discount_curve():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, 'test_data/discount_curve_rates_20231009.pickle')
     with open(file_path, 'rb') as f:
         dates_rates = pickle.load(f)
-        
+
+    assert sum([x[1] for x in dates_rates]) == pytest.approx(4.425019106569014)
+
     discount_curve = DiscountCurveZeroRates(
         anchor_date=datetime.date(2023, 10, 9),
         dates=[x[0] for x in dates_rates],
         rates=[x[1] for x in dates_rates],
+        spot_date=datetime.date(2023, 10, 9)
     )
-    
-    discount_curve.discount(datetime.date(2023, 10, 31)) == pytest.approx(0.9967383750912533)
-    discount_curve.discount(datetime.date(2025, 10, 31)) == pytest.approx(0.9022576658755044)
-    discount_curve.discount(datetime.date(2028, 10, 31)) == pytest.approx(0.7890060517290453)
-    discount_curve.discount(datetime.date(2035, 10, 31)) == pytest.approx(0.5597111228433488)
-    
+
+    assert discount_curve.discount(datetime.date(2023, 10, 10)) == pytest.approx(0.9998483072290358)
+    assert discount_curve.discount(datetime.date(2023, 10, 15)) == pytest.approx(0.9990878513334494)
+    assert discount_curve.discount(datetime.date(2023, 10, 31)) == pytest.approx(0.9966533032824298)
+    assert discount_curve.discount(datetime.date(2023, 12, 14)) == pytest.approx(0.9899236991398519)
+    assert discount_curve.discount(datetime.date(2024, 12, 14)) == pytest.approx(0.937780040455758)
+    assert discount_curve.discount(datetime.date(2030, 12, 14)) == pytest.approx(0.7070846804385454)
+
+    discount_curve = DiscountCurveZeroRates(
+        anchor_date=datetime.date(2023, 10, 9),
+        dates=[x[0] for x in dates_rates],
+        rates=[x[1] for x in dates_rates],
+        spot_date=datetime.date(2023, 10, 11)
+    )
+
+    assert discount_curve.discount(datetime.date(2030, 12, 5))  == pytest.approx(0.7079006399757061)
+    assert discount_curve.discount(datetime.date(2023, 12, 14)) == pytest.approx(0.9899271035639946)
+
+
+def test_real_discount_curve_annual_compounding():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, 'test_data/discount_curve_rates_20231009.pickle')
+    with open(file_path, 'rb') as f:
+        dates_rates = pickle.load(f)
+
+    assert sum([x[1] for x in dates_rates]) == pytest.approx(4.425019106569014)
+
+    discount_curve = DiscountCurveZeroRates(
+        anchor_date=datetime.date(2023, 10, 9),
+        dates=[x[0] for x in dates_rates],
+        rates=[x[1] for x in dates_rates],
+        spot_date=datetime.date(2023, 10, 11),
+        continuous_compounding=False
+    )
+
+    assert discount_curve.discount(datetime.date(2023, 12, 5))  == pytest.approx(0.9915392586857317)
+    assert discount_curve.discount(datetime.date(2025, 12, 14)) == pytest.approx(0.8975920708568521)
+    assert discount_curve.discount(datetime.date(2045, 12, 14)) == pytest.approx(0.31953615079478853)
+
 def test_discount_curve_zero_shifted():
     anchor_date = datetime.date(2023, 10, 9)
-    
-    dates       = [
-        datetime.date(2023, 12, 31),
-        datetime.date(2024, 12, 31),
-    ]
-    
-    rates      = [
-        .05,
-        .06
-    ]
+    dates       = [datetime.date(2023, 12, 31), datetime.date(2024, 12, 31),]
+    rates      = [.05, .06]
     
     discountCurve = DiscountCurveZeroRates(anchor_date, dates, rates)
     assert discountCurve.discount(datetime.date(2025, 12, 31)) == pytest.approx(.8727763175237739)
